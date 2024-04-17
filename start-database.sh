e016f17d37fe6c634155012776ef8c7ee5dca6d1
Ricardo Castro <soyricardodev@proton.me>
2024-04-17 16:57:09 -0400

    change db port

--- start-database.sh
+++ start-database.sh
@@ -49,5 +49,5 @@ docker run -d \
   --name $DB_CONTAINER_NAME \
   -e POSTGRES_PASSWORD="$DB_PASSWORD" \
   -e POSTGRES_DB=res-manage \
-  -p 5432:5432 \
+  -p 3001:5432 \
   docker.io/postgres && echo "Database container '$DB_CONTAINER_NAME' was successfully created"
