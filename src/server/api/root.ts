14c7efc06ff5ef3f437af1b31d62162970e0e768
Ricardo Castro <soyricardodev@proton.me>
2024-04-18 09:09:13 -0400

    add categoriesRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
-import { postRouter } from "~/server/api/routers/post";
+// import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+
+import { categoriesRouter } from "./routers/category";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +9,8 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  // post: postRouter,
+  category: categoriesRouter,
 });
 
 // export type definition of API
