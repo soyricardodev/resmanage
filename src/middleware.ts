8cda4e792889dbca90db5d13f89aff6f8984c8b8
Ricardo Castro <soyricardodev@proton.me>
2024-04-19 09:23:47 -0400

    fix: :bug: go back to default middleware recommenden by the docs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,17 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import {
+  clerkMiddleware,
+  createRouteMatcher,
+  getAuth,
+} from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export default clerkMiddleware(
-  (auth, req) => {
-    if (isProtectedRoute(req)) auth().protect();
-  },
-  {
-    afterSignInUrl: "/dashboard",
-    afterSignUpUrl: "/dashboard",
-  },
-);
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+  return NextResponse.next();
+});
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)", "/"],
 };
